d516593b388ec7797d551cde69a7913d3c042118
Can Kanbur <cankanbur1@gmail.com>
2022-11-21 10:06:24 +0300

    init

---
+++ dummy-data/Users.ts
@@ -0,0 +1,63 @@
+export default [
+  {
+    id: 'u1',
+    name: 'Irmik',
+    imageUri:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
+    status: 'Hello there, how are you',
+  },
+  {
+    id: 'u2',
+    name: 'Elon Musk',
+    imageUri:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+  },
+  {
+    id: 'u3',
+    name: 'Jeff',
+    imageUri:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
+  },
+  {
+    id: 'u4',
+    name: 'Zuck',
+    imageUri:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg',
+  },
+  {
+    id: 'u5',
+    name: 'Graham',
+    imageUri:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/graham.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Biahaze',
+    imageUri:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/biahaze.jpg',
+  },
+  {
+    id: 'u7',
+    name: 'Sus?',
+    imageUri:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+  },
+  {
+    id: 'u8',
+    name: 'Daniel',
+    imageUri:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/2.jpg',
+  },
+  {
+    id: 'u9',
+    name: 'Carlos',
+    imageUri:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/3.jpg',
+  },
+  {
+    id: 'u10',
+    name: 'Angelina Jolie',
+    imageUri:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/4.jpg',
+  },
+];
